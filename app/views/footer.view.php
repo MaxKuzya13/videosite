2d98ad668abae88b62cd22fc567ff08550a41edd
MaxKuzya13 <maxit2301@gmail.com>
2023-03-31 16:18:05 +0300

    First day

---
+++ app/views/footer.view.php
@@ -0,0 +1,55 @@
+<div class="class_1" >
+    <div class="class_26" >
+        <img src="assets/images/vendor-5.jpg" class="class_3" >
+        <h1  class="class_27" >
+            My Video Website
+        </h1>
+        <div  class="class_28" >
+            Copyright 2023. All rights reserved
+        </div>
+    </div>
+    <div class="class_29" >
+        <h4  class="class_30" >
+            Quick links:
+        </h4>
+        <a href="#"  class="class_31" >
+            Home
+        </a>
+        <a href="#"  class="class_32" >
+            Latest
+        </a>
+        <a href="#"  class="class_32" >
+            Popular
+        </a>
+    </div>
+    <div class="class_29" >
+        <h4  class="class_30" >
+            User links:
+        </h4>
+        <a href="#"  class="class_31" >
+            Admin
+        </a>
+        <a href="#"  class="class_32" >
+            Login
+        </a>
+        <a href="#"  class="class_32" >
+            Signup
+        </a>
+    </div>
+    <div class="class_29" >
+        <h4  class="class_30" >
+            More links:
+        </h4>
+        <a href="#"  class="class_31" >
+            Upload video
+        </a>
+        <a href="#"  class="class_32" >
+            New Playlist
+        </a>
+    </div>
+</div>
+</section>
+
+</body>
+</html>
+
---
+++ app/views/header.view.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>My video website</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap-icons.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css?2173">
+</head>
+<body>
+
+<div class="class_1" >
+    <div class="class_2" >
+        <img src="assets/images/vendor-5.jpg" class="class_3" >
+    </div>
+    <div class="class_4" >
+        <div class="class_5" >
+            <div class="class_6" >
+                <h1  class="class_7" >
+                    My Video Website
+                </h1>
+            </div>
+            <div class="class_8" >
+                <i class="bi bi-facebook class_9">
+                </i>
+                <i  class="bi bi-twitter class_9">
+                </i>
+                <i  class="bi bi-youtube class_9">
+                </i>
+            </div>
+        </div>
+        <div class="class_10" >
+            <a href="#"  class="class_11" >
+                Home
+            </a>
+            <a href="#"  class="class_12" >
+                Latest
+            </a>
+            <a href="#"  class="class_12" >
+                Popular
+            </a>
+            <a href="#"  class="class_12" >
+                Playlists
+            </a>
+        </div>
+    </div>
+</div>
