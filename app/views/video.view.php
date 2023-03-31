f1ae532703ae0cc01eb1043048eb1a1cb2c53437
MaxKuzya13 <maxit2301@gmail.com>
2023-03-31 20:19:43 +0300

    First day

--- app/views/video.view.php
+++ app/views/video.view.php
@@ -1,19 +1,30 @@
 <?php $this->view('header'); ?>
 
     <section class="class_60" >
-        <form method="post" enctype="multipart/form-data" class="class_61" >
-            <h1  class="class_25" >
+        <form onsubmit="submit_form(event)" method="post" enctype="multipart/form-data" class="class_61" >
+            <h1 class="class_25" >
                 Upload video
             </h1>
-            <img src="<?=ROOT?>/assets/images/pexels-photo-3756774.jpeg" class="class_62" >
-            <input type="file" name="image"  class="class_63">
+            <label>
+                <div>Featured Image:</div>
+                <img src="<?=get_image()?>" class="class_62" style="cursor: pointer" >
+                <input type="file" name="image"  class="class_63">
+            </label>
             <div class="class_64" >
                 <div class="class_65" >
                     <label  class="class_66" >
                         Title:
                     </label>
-                    <input placeholder="Title" type="text" name="title" class="class_67" >
+                    <input value="<?=old_value('title')?>" placeholder="Title" type="text" name="title" class="class_67" >
                 </div>
+
+                <div class="class_65" >
+                    <label  class="class_66" >
+                        Video File:
+                    </label>
+                    <input type="file" name="file" class="class_63" style="display: block;">
+                </div>
+
                 <div class="class_68" >
                     <label  class="class_69" >
                         Playlist:
@@ -34,8 +45,7 @@
                     <label class="class_72" >
                         Description:
                     </label>
-                    <textarea placeholder="Description" name="description" class="class_73" >
-						</textarea>
+                    <textarea placeholder="Description" name="description" class="class_73"><?=old_value('description')?></textarea>
                 </div>
             </div>
             <div class="class_74" >
@@ -49,7 +59,7 @@
                 <button  class="class_77" >
                     Save
                 </button>
-                <button  class="class_78" >
+                <button type="button" class="class_78" >
                     Cancel
                 </button>
                 <div class="class_79" >
@@ -59,3 +69,49 @@
     </section>
 
 <?php $this->view('footer'); ?>
+
+<script>
+
+    var image_added = false;
+    var file_added = false:
+
+    function submit_form(e)
+    {
+        e.preventDefault()
+
+        let myform = e.currentTarget;
+        let inputs = myform.querySelectorAll('input, select, textarea');
+        let data = new FormData();
+        let optional_fields = ['description', 'playlist_id', 'image', 'file'];
+
+        if(!image_added)
+        {
+            console.log('qq');
+            alert('Please add a valid image');
+            return;
+        }
+        if(!file_added)
+        {
+            alert('Please add a valid video file');
+            return;
+        }
+
+        for (var i = inputs.length - 1; i >= 0; i--)
+        {
+            if(inputs[i].value == '' && optional_fields.includes(inputs[i].name))
+            {
+                // empty value
+                alert("The field: "+inputs[i].name+" is required!");
+                return;
+            }
+            if(inputs[i].name == 'image' || inputs[i].name == 'file')
+            {
+                data.append(inputs[i].name, inputs[i].files[0]);
+            } else {
+                data.append(inputs[i].name, inputs[i].value);
+            }
+
+        }
+    }
+
+</script>
